7099de43a16a1a00952eef7e27e8bad24c9c7b9c
Lorenzo Spadaro <spadaros.lorenzo@gmail.com>
2025-05-14 18:08:42 +0200

    added ssl

--- model/pool.js
+++ model/pool.js
@@ -6,6 +6,7 @@ const pool=new Pool({
     port:5432,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    ssl:{require:true},
 })
 module.exports=pool;
